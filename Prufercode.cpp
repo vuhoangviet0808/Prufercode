8b5fda043201056390dd6305e87f58aa99d1ea46
vuhoangviet0808 <118671392+vuhoangviet0808@users.noreply.github.com>
2023-05-08 14:50:13 +0700

    Update Prufercode.cpp

--- Prufercode.cpp
+++ Prufercode.cpp
@@ -1,5 +1,9 @@
 #include<stdio.h>
 
+
+// Ý tưởng là sử dụng ma trận kề để lưu các đỉnh kề nhau và từ đó tính tổng bậc của các đỉnh
+
+
 int pruf;
 int sum(int a[],int n){
 	int sum=0;
@@ -10,13 +14,11 @@ int sum(int a[],int n){
 	return sum;
 }
 
-
+int a[100000][100000] = {0}; 
 
 int main(){
 	int n;
 	scanf("%d",&n);
-	int a[n+1][n+1] = {0};
-	
 	
 	for(int i=0;i<n;i++){
 		int u,v;
@@ -28,7 +30,7 @@ int main(){
 	
 	
 	while(n != 1){
-		for(int i = 0;i<= m; i++){
+		for(int i = 1;i<= m; i++){
 			if(sum(a[i],m) == 1){
 				printf("%d ",pruf);
 				a[i][pruf] = 0;
